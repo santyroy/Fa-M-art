7d1ee65a190f8e912d639cf275401b848a08832f
sougata <sougata.roy26@yahoo.com>
2018-11-13 01:22:03 +0530

    Added Login and Logout functionality

--- app/src/main/java/com/example/sougata/famart/SignInActivity.java
+++ app/src/main/java/com/example/sougata/famart/SignInActivity.java
@@ -1,8 +1,10 @@
 package com.example.sougata.famart;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -10,49 +12,67 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Locale;
+
 public class SignInActivity extends AppCompatActivity {
 
-    private EditText name;
+    private EditText email;
     private EditText password;
     private TextView attempts;
     private TextView register;
     private Button login;
+    private ProgressDialog progressDialog;
     private int countAttempts = 5;
+
+    //Add Firebase authentication service
+    private FirebaseAuth firebaseAuth;
+
+    //get the String value for attempts
+    private String attemptString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-        name = findViewById(R.id.name_et);
+        email = findViewById(R.id.name_et);
         password = findViewById(R.id.password_et);
         attempts = findViewById(R.id.attempts_tv);
         login = findViewById(R.id.login_btn);
         register = findViewById(R.id.register_tv);
-        final String s = SignInActivity.this.getString(R.string.no_of_incorrect_attempts);
-        attempts.setText(s + countAttempts);
+        attemptString = SignInActivity.this.getString(R.string.no_of_incorrect_attempts);
+        attempts.setText(String.format(Locale.getDefault(), "%s%d", attemptString, countAttempts));
+
+        //Instantiating Firebase auth component
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(SignInActivity.this);
+
+        //Getting the current user from Firebase
+        //Check with the database if a user is already logged into the app
+        //if the user has logged in we must take the user to next activity without
+        //asking for logging again
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user != null) {
+            //destroys current activity
+            finish();
+            //starting the next activity
+            startActivity(new Intent(SignInActivity.this, WelcomeActivity.class));
+        }
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!(name.getText().toString().isEmpty() && password.getText().toString().isEmpty())) {
-                    String n = name.getText().toString();
-                    String p = password.getText().toString();
-                    if (n.equals("admin") && p.equals("1234")) {
-                        Intent intent = new Intent(SignInActivity.this, WelcomeActivity.class);
-                        startActivity(intent);
-                    } else {
-                        if (countAttempts >= 0) {
-                            countAttempts--;
-                            attempts.setText(s + countAttempts);
-                            if (countAttempts == 0) {
-                                login.setEnabled(false);
-                            }
-                        }
-                    }
-                } else {
-                    Toast.makeText(SignInActivity.this, "Please enter your credentials", Toast.LENGTH_SHORT).show();
-                }
+
+                String e = email.getText().toString();
+                String p = password.getText().toString();
+                validate(e, p);
             }
         });
 
@@ -67,7 +87,54 @@ public class SignInActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        Log.i("MY LOG : ","Back button pressed");
+        Log.i("MY LOG : ", "Back button pressed");
         finish();
+    }
+
+    public void validate(String email, String password) {
+
+        //Check if the field are not empty/blank
+        if (signinCheck(email, password)) {
+            //start progress dialog
+            progressDialog.setMessage("Verifying...");
+            progressDialog.show();
+            firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        progressDialog.dismiss();
+                        Toast.makeText(SignInActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(SignInActivity.this, WelcomeActivity.class));
+                    } else {
+                        progressDialog.dismiss();
+                        Toast.makeText(SignInActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                        countAttempts--;
+                        attempts.setText(String.format(Locale.getDefault(), "%s%d", attemptString, countAttempts));
+                        if (countAttempts == 0) {
+                            login.setEnabled(false);
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    private boolean signinCheck(String uemail, String upass) {
+
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+        if (uemail.isEmpty() || upass.isEmpty()) {
+            Toast.makeText(this, "Please enter all the details", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if (!uemail.matches(emailPattern)) {
+            Toast.makeText(this, "Email should valid format", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if (upass.length() < 6) {
+            Toast.makeText(this, "Password should be at least 6 characters", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
     }
 }
--- app/src/main/java/com/example/sougata/famart/WelcomeActivity.java
+++ app/src/main/java/com/example/sougata/famart/WelcomeActivity.java
@@ -1,21 +1,76 @@
 package com.example.sougata.famart;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    //Initializing the views
+    private Button logout;
+
+    //Create Firebase auth
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
+        //Instantiating Firebase
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //Instantiating the views
+        logout = findViewById(R.id.logout_btn);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logoutMethod();
+            }
+        });
     }
 
     @Override
-    public void onBackPressed() {
-        Log.i("MY LOG : ","Back button pressed");
-        finish();
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.logout_menu:
+                logoutMethod();
+                break;
+
+            case R.id.settings_menu:
+                //TODO: Add settings option
+                break;
+
+            case R.id.refresh_menu:
+                //TODO: Add refresh option
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void logoutMethod() {
+        String email = firebaseAuth.getCurrentUser().getEmail();
+        Log.i("EMAIL : ", email);
+        if (email != null) {
+            firebaseAuth.signOut();
+            finish();
+            startActivity(new Intent(WelcomeActivity.this, SignInActivity.class));
+            Toast.makeText(WelcomeActivity.this, "Logout successful", Toast.LENGTH_SHORT).show();
+        }
     }
 }
